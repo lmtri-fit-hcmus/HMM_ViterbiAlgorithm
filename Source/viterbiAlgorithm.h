5f6b488ecfaee01fd728718cc2d7ca8e32f7222e
Minh-Tri Le <minhtrikhtnk20@gmail.com>
2022-05-02 09:58:35 +0700

    clean code 1

---
+++ Source/viterbiAlgorithm.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+double GetEmissionProb(string Observation, int j, vector<vector<double>> Emission, vector<string> O);
+void PrintPath(int maxIndex, vector<vector<int>> Path, vector<string> States);
+void ViterbiAlgorithm(vector<string> Observations, vector<string> O, vector<string> States, vector<double> Pi, vector<vector<double>> Trans, vector<vector<double>> Emission);
