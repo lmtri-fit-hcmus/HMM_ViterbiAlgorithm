18afdfd7f6dcb1152e34f0cf58022b55621b76e7
Minh-Tri Le <minhtrikhtnk20@gmail.com>
2022-05-10 11:22:39 +0700

    POS_Tagging initialize

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,7 +11,7 @@ int main()
     }
     vector<string> O{"1", "2", "3", "4", "5", "6"};
     vector<string> States{"Balance", "Inbalance"};
-    vector<double> Pi = {0.1, 0.9};
+    vector<double> Pi = {0, 1};
     vector<vector<double>> Trans = {{0.8, 0.2}, {0.7, 0.3}};
     vector<vector<double>> Emission = {{1.0 / 6, 1.0 / 6, 1.0 / 6, 1.0 / 6, 1.0 / 6, 1.0 / 6}, {0.1, 0.1, 0.1, 0.1, 0.1, 0.5}};
     ViterbiAlgorithm(Observations, O, States, Pi, Trans, Emission);
